be0a653a53f56dfe2058b5d842e0535851847a61
aditigawari <aditigawari.2005@gmail.com>
2026-08-11 20:35:45 +0530

    Initial CI project

---
+++ README.md
@@ -0,0 +1,19 @@
+# CI Jenkins Multi Build Demo
+
+This project is used for demonstrating Jenkins Continuous Integration with:
+
+- Maven
+- Ant
+- Gradle
+
+## Maven
+
+mvn -B clean verify
+
+## Ant
+
+ant build
+
+## Gradle
+
+gradle clean build
---
+++ verify-tools.bat
@@ -0,0 +1,32 @@
+@echo off
+echo ===========================
+echo Java
+echo ===========================
+java -version
+javac -version
+
+echo.
+echo ===========================
+echo Git
+echo ===========================
+git --version
+
+echo.
+echo ===========================
+echo Maven
+echo ===========================
+mvn -version
+
+echo.
+echo ===========================
+echo Ant
+echo ===========================
+ant -version
+
+echo.
+echo ===========================
+echo Gradle
+echo ===========================
+gradle -version
+
+pause
